cc8a920e9c0e99ea5f55d97a4774940d8c4cbee1
krishprasad63 <krishprasad.kp7@gmail.com>
2024-10-13 23:01:03 +0530

    some_changes

--- A/Access.py
+++ A/Access.py
@@ -3,6 +3,10 @@ import string
 import shutil
 
 #Write any fixed address, as per your choice and create a new folder named 'random' in the path
+''' As this code is to access, so for accessing there should be files you will be accessing. So, to generate files run Generate.py.
+As you already generated the file, then you must be having fixed_address and random file inside it in which there will be files which is to be accessed. 
+So, set fixed_address, same as of Generate.py and run this code.'''
+
 fixed_address = 'C:/Users/krish/PythonTask'
 
 folder_path = fixed_address+'/random' 
@@ -22,9 +26,10 @@ for i in range(len(a)):
 print(list_)
 
 for k in list_:
-    new_folder =  fixed_address + k
+    new_folder =  fixed_address +'/'+ k
     if not os.path.exists(new_folder): 
         os.makedirs(new_folder) 
  
 for j in range(len(a)):
-    os.rename(fixed_address+'/random/{}'.format(a[j]), fixed_address+'/{}/{}'.format(f_ext(a[j]),a[j]))
+    #every file will be moved from 'random' folder to the satisfying extension folders above created 
+    os.rename(folder_path + a[j] , fixed_address + '/{}/{}'.format(f_ext(a[j]),a[j]))
--- A/Generate.py
+++ A/Generate.py
@@ -5,6 +5,10 @@ import string
 ext = ['.txt','.doc','.xls','.py','.c','.html','.ppt','.cpp','.css','.js']
 
 #Write any fixed address, as per your choice and create a new folder named 'random' in the path
+'''For simplicity, go to your c file then users then your respective name file, and then create folder name PythonTask.
+Copy this address and set it to fixed_address variable. Now, in 'PythonTask' folder create folder named 'random'.
+This code will be generating n files of random extension from the ext list give above.'''
+
 fixed_address = 'C:/Users/krish/PythonTask'
 folder_path = fixed_address +'/random' 
 
